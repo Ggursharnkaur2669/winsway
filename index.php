380c134c331c700578b94d6baf885b05305bb3f5
Ggursharnkaur2669 <55241915+Ggursharnkaur2669@users.noreply.github.com>
2020-05-10 19:50:34 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,442 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Winsway Transport Ltd.</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Arbutus+Slab|Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+
+
+  <div class="site-wrap">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+    <header class="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+
+      <div class="container-fluid">
+        <div class="row align-items-center justify-content-center">
+          
+          <div class="">
+            <nav class="site-navigation position-relative text-right" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="#home-section" class="nav-link">Home</a></li>
+                <li><a href="#services-section" class="nav-link">Services</a></li>
+                <li><a href="#about-section" class="nav-link">About</a></li>
+                
+              </ul>
+            </nav>
+          </div>
+
+          <div class=" text-center mx-5">
+            <h1 class="m-0 site-logo"><a href="index.php">Me.</a></h1>
+          </div>
+
+          <div class="text-left">
+
+            <nav class="site-navigation position-relative" role="navigation">
+              <ul class="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                <li><a href="#portfolio-section" class="nav-link">Partners</a></li>
+                <li><a href="#clients-section" class="nav-link">Client</a></li>
+                <li><a href="#contact-section" class="nav-link">Contact</a></li>
+              </ul>
+            </nav>
+
+
+            <div class="d-inline-block d-lg-none" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle float-right"><span class="icon-menu h3"></span></a></div>
+
+          </div>
+
+        </div>
+      </div>
+      
+    </header>
+
+
+    <div class="site-blocks-cover overlay bg-light" id="home-section">
+
+      <div class="container">
+        <div class="row justify-content-center">
+
+          <div class="col-md-12 mt-lg-5 text-left align-self-center text-intro">
+            <div class="row">
+              <div class="col-lg-6">
+                <h1 class="text-black">Winsway Heavy Transport Services</h1>
+                <p class="lead">We are LTL, FTL, EXPEDITE, 3PL, CROSSDOCKING, WAREHOUSING & STORAGE Based on Brampton, Ontario</p>
+                <p><a href="#portfolio-section" class="btn smoothscroll btn-primary">Portfolio</a></p>
+
+              </div>
+            </div>
+          </div>
+            
+        </div>
+      </div>
+
+      <img src="images/truck.jpg" alt="Image" class="img-face" data-aos="fade" width="650" height="500" style="vertical-align:middle; border:5px solid black">
+
+      
+    </div>  
+
+    
+
+    
+    <div class="site-section" id="services-section">
+      <div class="container">
+        <div class="row ">
+          <div class="col-12 mb-5 position-relative">
+            <h2 class="section-title text-center mb-5">Our Services</h2>
+          </div>
+          
+          <div class="col-md-6 mb-4">
+            <div class="service d-flex h-100">
+              <div class="service-number mr-4"><span class="icon-style"></span></div>
+              <div class="service-about">
+                <h3>Heavy Loads</h3>
+                <p>Loads considered heavy are shipped to varied areas of the country and across the border.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <div class="service d-flex h-100">
+              <div class="service-number mr-4"><span class="icon-business_center"></span></div>
+              <div class="service-about">
+                <h3>Light Loads</h3>
+                <p>Loads considered Light are shipped to varied areas of the country and across the border.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <div class="service d-flex h-100">
+              <div class="service-number mr-4"><span class="icon-desktop_windows"></span></div>
+              <div class="service-about">
+                <h3>Flat Bed Services</h3>
+                <p>Highly experienced in providing flat bed services.</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-6 mb-4">
+            <div class="service d-flex h-100">
+              <div class="service-number mr-4"><span class="icon-layers"></span></div>
+              <div class="service-about">
+                <h3>Packinging &amp; Repackaging </h3>
+                <p>We exclusively provide packaging and repackaging services.</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section" id="about-section">
+      <div class="container">
+        <div class="row ">
+          <div class="col-12 mb-4 position-relative">
+            <h2 class="section-title">About Us</h2>
+          </div>
+          <div class="col-lg-4 order-1 order-lg-2 mb-4 mb-lg-0">
+            <div class="bg-light pt-5">
+            <img src="images/truck.jpg" alt="Image" class="img-fluid" height="300" width="200">
+            </div>
+          </div>
+          <div class="col-lg-4 order-2 order-lg-1">
+            <p>We are experienced in highly effective services of loading, unloading.</p>
+            <p>Hassle free services under one roof. </p>
+          </div>
+          <div class="col-lg-4 order-3 order-lg-3">
+            <p>Customer convenience is our main motto. Our drivers are highly professional in delivery across the borders.</p>
+            <p>Tracked record of each and every delivery without any fail.</p>
+            <p><a href="#contact-section" class="btn smoothscroll btn-primary">Contact Me</a></p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+
+
+
+
+    <section class="site-section block__62272" id="portfolio-section">
+      
+
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-12 position-relative">
+            <h2 class="section-title text-center mb-5">Our Partners</h2>
+          </div>
+        </div>
+
+        
+        <div id="posts" class="row no-gutter">
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_2.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_2.jpg">
+            </a>
+          </div>
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_3.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_3.jpg">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_4.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_4.jpg">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+
+            <a href="images/post_5.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_5.jpg">
+            </a>
+
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_6.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_6.jpg">
+            </a>
+          </div>
+
+          <div class="item brand col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_2.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_2.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_3.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_3.jpg">
+            </a>
+          </div>
+
+          <div class="item design col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_4.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_4.jpg">
+            </a>
+          </div>
+
+          <div class="item web col-6 col-sm-6 col-md-6 col-lg-4 col-xl-4 mb-4">
+            <a href="images/post_6.jpg" class="item-wrap fancybox">
+              <span class="icon-search2"></span>
+              <img class="img-fluid" src="images/post_6.jpg">
+            </a>
+          </div>
+
+          
+        </div>
+      </div>
+
+    </section>
+
+
+    <section class="site-section bg-dark">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 mb-5 position-relative">
+            <h2 class="section-title text-center mb-5 text-white">testimonials</h2>
+          </div>
+        </div>
+        <div class="owl-carousel slide-one-item">
+          <div class="slide">
+            <blockquote>
+              <p>Highly Specialized and trained drivers.</p>
+              <p><cite>&mdash; xyz.</cite></p>
+            </blockquote>
+          </div>
+          <div class="slide">
+            <blockquote>
+              <p>Work efficent staff and operators. Stern and committed to deadlines </p>
+              <p><cite>&mdash; abc</cite></p>
+            </blockquote>
+          </div>
+          <div class="slide">
+            <blockquote>
+              <p>Certified for hizzardious substances delivery.</p>
+              <p><cite>&mdash; Tarwinz Niche Technology Vision</cite></p>
+            </blockquote>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="site-section"  id="clients-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 mb-5 position-relative">
+            <h2 class="section-title text-center">Clients</h2>
+          </div>
+          <div class="col-6 col-sm-6 col-md-4 col-lg-6 text-center">
+            <img src="images/logo_1.png" alt="Image" class="img-fluid">
+          </div>
+          <div class="col-6 col-sm-6 col-md-4 col-lg-6 text-center">
+            <img src="images/logo_2.jpg" alt="Image" class="img-fluid">
+          </div>
+         
+          
+        </div>
+      </div>
+    </section>
+
+    
+
+    <section class="site-section" id="contact-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 mb-5 position-relative">
+            <h2 class="section-title text-center mb-5">Contact Form</h2>
+          </div>
+        </div>
+        <form action="#" class="form">
+          <div class="row mb-4">
+            <div class="form-group col-6">
+              <input type="text" class="form-control" placeholder="First name">
+            </div>
+            <div class="form-group col-6">
+              <input type="text" class="form-control" placeholder="Last name">
+            </div>
+          </div>
+
+          <div class="row mb-4">
+            <div class="form-group col-12">
+              <input type="email" class="form-control" placeholder="Email address">
+            </div>
+          </div>
+
+          <div class="row mb-4">
+            <div class="form-group col-12">
+              <input type="text" class="form-control" placeholder="Subject of the message">
+            </div>
+          </div>
+
+          <div class="row mb-4">
+            <div class="form-group col-12">
+              <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Type your message here.."></textarea>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <input type="submit" class="btn btn-primary" value="Send Message">
+            </div>
+          </div>
+          
+        </form>
+      </div>
+    </section>
+
+    <footer class="site-section bg-light footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-3">
+            <h3 class="footer-title">Address</h3>
+            <p><span class="d-inline-block d-md-block">XyZ company,</span> Brampton,Ontario, Canada</p>
+          </div>
+          <div class="col-md-5 mx-auto">
+            <div class="row">
+              <div class="col-lg-4">
+                <h3 class="footer-title">Services</h3>
+                <ul class="list-unstyled">
+                  <li><a href="#">Web App</a></li>
+                  <li><a href="#">Graphic</a></li>
+                  <li><a href="#">Branding</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-4">
+                <h3 class="footer-title">Resources</h3>
+                <ul class="list-unstyled">
+                  <li><a href="#">Web App</a></li>
+                  <li><a href="#">Graphic</a></li>
+                  <li><a href="#">Branding</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-4">
+                <h3 class="footer-title">Links</h3>
+                <ul class="list-unstyled">
+                  <li><a href="#">Web App</a></li>
+                  <li><a href="#">Graphic</a></li>
+                  <li><a href="#">Branding</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <h3 class="footer-title">Follow Me</h3>
+            <a href="#" class="social-circle"><span class="icon-twitter"></span></a>
+            <a href="#" class="social-circle"><span class="icon-facebook"></span></a>
+            <a href="#" class="social-circle"><span class="icon-instagram"></span></a>
+            <a href="#" class="social-circle"><span class="icon-dribbble"></span></a>
+            <a href="#" class="social-circle"><span class="icon-linkedin"></span></a>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12 text-center">
+          <p>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |  <i class="icon-heart text-danger" aria-hidden="true"></i> designed by <a href="mailto:Ggursharankaur@gmail.com">Gursharan Kaur</a>
+            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+            </div>
+        </div>
+      </div>
+    </footer>
+
+  </div> <!-- .site-wrap -->
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.fancybox.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/isotope.pkgd.min.js"></script>
+
+  
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
